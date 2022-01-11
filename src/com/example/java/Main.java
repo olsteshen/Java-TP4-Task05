8514e5ed2b37702d0d5c94f9c96e019e1a285428
stesho01 <Listo4ek>
2022-01-11 15:22:03 +0200

    fix Variant A solution

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         String[] arr1 = {"1", "2", "3", "4", "5", "6", "7"};
-        String[] arr2 = {"Book1", "Book2", "Book3", "Book4", "Book5", "Book6", "Book7", "Book8"};
+        String[] arr2 = {"Book1", "Book2", "Book3", "Book4", "Book5"};
         Integer arr3[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         Integer arr4[] = {100, 200, 300, 400, 500, 600, 700, 800};
 
@@ -18,7 +18,7 @@ public class Main {
         //variant 1: if K > V -  write null to the value,if K < V  - do not process extra values
         Map<String, String> newmap1 = conv.convertVarB(arr1, arr2);
         System.out.println("==Variant 1==");
-        System.out.println("Map 1 (arr1 as keys and arr2 as values) :" + newmap1);
+        System.out.println("Map 1 (arr1 as keys and arr2 as values):" + newmap1);
         //variant 2: if K > V - "Value is not enough for map" error; if K < V - "Key is not enough for map" error
         System.out.println("==Variant 2==");
         Map<String, String> newmap2 = conv.convertVarA(arr1, arr2);
@@ -27,6 +27,5 @@ public class Main {
         System.out.println("Map 3 (arr3 as keys and arr4 as values):" + newmap3);
         Map<Integer, String> newmap4 = conv.convertVarA(arr3, arr2);
         System.out.println("Map 4 (arr3 as keys and arr2 as values):" + newmap4);
-
     }
 }
